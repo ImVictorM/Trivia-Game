9d27a58d5c5d3d934a2159de948711ff18ec8d57
Victor Mendes <victorutdsim7@gmail.com>
2024-04-01 14:40:22 -0300

    refactor redux to use redux toolkit

---
+++ src/redux/slices/playerSlice.ts
@@ -0,0 +1,39 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+type PlayerState = {
+  name: string;
+  assertions: number;
+  score: number;
+  gravatarEmail: string;
+};
+
+const initialPlayerState: PlayerState = {
+  name: "",
+  assertions: 0,
+  score: 0,
+  gravatarEmail: "",
+};
+
+const playerSlice = createSlice({
+  name: "player",
+  initialState: initialPlayerState,
+  reducers: {
+    login: (
+      state,
+      action: PayloadAction<{ name: string; gravatarEmail: string }>
+    ) => {
+      state.name = action.payload.name;
+      state.gravatarEmail = action.payload.gravatarEmail;
+    },
+    setScore: (state, action) => {
+      state.score = action.payload;
+    },
+    setAssertions: (state, action: PayloadAction<number>) => {
+      state.assertions = action.payload;
+    },
+  },
+});
+
+export const { login, setAssertions, setScore } = playerSlice.actions;
+
+export const playerReducer = playerSlice.reducer;
