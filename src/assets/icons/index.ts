fbbd2133e3296d1e96c06f358678f8b02d69f794
Victor Mendes <victorutdsim7@gmail.com>
2024-04-04 20:17:08 -0300

    add new icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,4 @@
 export { default as settingsCog } from "./settings-cog.svg";
 export { default as loadingIcon } from "./loading-icon.svg";
+export { default as star } from "./star.svg";
+export { default as timer } from "./timer.svg";
