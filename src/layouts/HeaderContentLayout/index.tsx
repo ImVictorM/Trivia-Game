6805de7d96bd6b747bbe19d743336954a05b1061
Victor Mendes <victorutdsim7@gmail.com>
2024-04-05 20:36:36 -0300

    refactor layout components structure

--- src/layouts/HeaderContentLayout/index.tsx
+++ src/layouts/HeaderContentLayout/index.tsx
@@ -1,4 +1,4 @@
-import { Header } from "./components";
+import Header from "./Header";
 import { PurpleBoxBackground, StyledContent, StyledLayout } from "./style";
 
 type HeaderContentLayoutProps = {
