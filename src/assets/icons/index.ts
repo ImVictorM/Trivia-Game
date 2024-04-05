69a1e710ed3457e79dbcb45e68f0718b4e7ef805
Victor Mendes <victorutdsim7@gmail.com>
2024-04-05 20:35:33 -0300

    add more icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,4 +1,7 @@
 export { default as settingsCog } from "./settings-cog.svg";
-export { default as loadingIcon } from "./loading-icon.svg";
+export { default as spinnerLoadingIcon } from "./spinner-loading-icon.svg";
 export { default as star } from "./star.svg";
 export { default as timer } from "./timer.svg";
+export { default as incorrectAnswerIcon } from "./incorrect-answer-icon.svg";
+export { default as correctAnswerIcon } from "./correct-answer-icon.svg";
+export { default as dualBallLoadingIcon } from "./dual-ball-loading.svg";
