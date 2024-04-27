59f857bcf3df0fc3c37a282ff36da996b15830c6
Victor Mendes <victorutdsim7@gmail.com>
2024-04-27 16:54:56 -0300

    fix: importing in main file

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { setupStore } from "./redux/store";
 import "./services/i18next";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
+      <Provider store={setupStore()}>
         <React.Suspense>
           <App />
         </React.Suspense>
