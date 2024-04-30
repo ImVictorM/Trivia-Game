bb06bf3176ca4d7756504452c1dc562b9f3e170d
Victor Mendes <victorutdsim7@gmail.com>
2024-04-30 17:14:13 -0300

    test: answer button

---
+++ src/pages/Game/AnswerButton/AnswerButton.test.tsx
@@ -0,0 +1,125 @@
+import { renderWithProviders } from "@/tests/utils";
+import AnswerButton, {
+  ANSWER_BUTTON_COMPONENT_ANSWER_ID,
+  ANSWER_BUTTON_COMPONENT_CORRECT_ICON_ID,
+  ANSWER_BUTTON_COMPONENT_ID,
+  ANSWER_BUTTON_COMPONENT_LETTER_ID,
+  ANSWER_BUTTON_COMPONENT_WRONG_ICON_ID,
+} from ".";
+import { GET_QUESTIONS_SUCCESS } from "@/tests/mocks/triviaApi";
+import { act, screen } from "@testing-library/react";
+
+describe("Game page: AnswerButton component", () => {
+  const question = GET_QUESTIONS_SUCCESS.results[0];
+
+  it("Renders correctly when answers was not selected", () => {
+    const answers = [question.correct_answer, ...question.incorrect_answers];
+
+    const letterPerIndex: Record<number, string> = {
+      0: "A",
+      1: "B",
+      2: "C",
+      3: "D",
+    };
+
+    renderWithProviders(
+      <>
+        {answers.map((answer, index) => (
+          <AnswerButton
+            correctAnswer={question.correct_answer}
+            answerWasSelected={false}
+            answer={answer}
+            index={index}
+            key={index}
+          />
+        ))}
+      </>
+    );
+
+    const buttons = screen.getAllByTestId(ANSWER_BUTTON_COMPONENT_ID);
+    const letters = screen.getAllByTestId(ANSWER_BUTTON_COMPONENT_LETTER_ID);
+    const answersFromUi = screen.getAllByTestId(
+      ANSWER_BUTTON_COMPONENT_ANSWER_ID
+    );
+    const correctIcon = screen.queryByTestId(
+      ANSWER_BUTTON_COMPONENT_CORRECT_ICON_ID
+    );
+    const wrongIcon = screen.queryByTestId(
+      ANSWER_BUTTON_COMPONENT_WRONG_ICON_ID
+    );
+
+    expect(buttons).toHaveLength(4);
+
+    letters.forEach((letter, index: number) => {
+      expect(letter.textContent).toEqual(letterPerIndex[index]);
+    });
+
+    answersFromUi.forEach((answer, index) => {
+      expect(answer.textContent).toEqual(answers[index]);
+    });
+
+    expect(correctIcon).not.toBeInTheDocument();
+    expect(wrongIcon).not.toBeInTheDocument();
+  });
+
+  it("Renders correctly when answer was select and is correct", () => {
+    renderWithProviders(
+      <AnswerButton
+        correctAnswer={question.correct_answer}
+        answerWasSelected={true}
+        answer={question.correct_answer}
+        index={1}
+      />
+    );
+
+    const button = screen.getByTestId(ANSWER_BUTTON_COMPONENT_ID);
+    const correctIcon = screen.getByTestId(
+      ANSWER_BUTTON_COMPONENT_CORRECT_ICON_ID
+    );
+
+    expect(button).toContainElement(correctIcon);
+  });
+
+  it("Renders correctly when answer was select and is wrong", () => {
+    renderWithProviders(
+      <AnswerButton
+        correctAnswer={question.correct_answer}
+        answerWasSelected={true}
+        answer={question.incorrect_answers[0]}
+        index={1}
+      />
+    );
+
+    const button = screen.getByTestId(ANSWER_BUTTON_COMPONENT_ID);
+    const correctIcon = screen.getByTestId(
+      ANSWER_BUTTON_COMPONENT_WRONG_ICON_ID
+    );
+
+    expect(button).toContainElement(correctIcon);
+  });
+
+  it("Fires the onClick functions correctly", async () => {
+    const onClickMock = vi.fn();
+    const { user } = renderWithProviders(
+      <AnswerButton
+        correctAnswer={question.correct_answer}
+        answerWasSelected={true}
+        answer={question.correct_answer}
+        index={1}
+        onClick={onClickMock}
+      />
+    );
+
+    const button = screen.getByTestId(ANSWER_BUTTON_COMPONENT_ID);
+
+    await act(async () => {
+      await user.click(button);
+    });
+
+    expect(onClickMock).toHaveBeenCalledOnce();
+    expect(onClickMock).toHaveBeenCalledWith(
+      expect.objectContaining({ type: "click" }),
+      question.correct_answer
+    );
+  });
+});
--- src/pages/Game/AnswerButton/index.tsx
+++ src/pages/Game/AnswerButton/index.tsx
@@ -7,12 +7,20 @@ type AnswerButtonProps = {
   answer: string;
   answerWasSelected: boolean;
   index: number;
-  disabled: boolean;
-  onClick: (
+  disabled?: boolean;
+  onClick?: (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
     answer: string
   ) => void;
 };
+
+export const ANSWER_BUTTON_COMPONENT_ID = "answer-button";
+export const ANSWER_BUTTON_COMPONENT_LETTER_ID = "answer-button-letter";
+export const ANSWER_BUTTON_COMPONENT_ANSWER_ID = "answer-button-answer";
+export const ANSWER_BUTTON_COMPONENT_CORRECT_ICON_ID =
+  "answers-button-wrong-icon";
+export const ANSWER_BUTTON_COMPONENT_WRONG_ICON_ID =
+  "answers-button-wrong-icon";
 
 export default function AnswerButton({
   correctAnswer,
@@ -20,19 +28,19 @@ export default function AnswerButton({
   answerWasSelected,
   index,
   onClick,
-  disabled,
+  disabled = false,
 }: AnswerButtonProps) {
   const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    onClick(e, answer);
+    if (onClick) {
+      onClick(e, answer);
+    }
   };
   const { t } = useTranslation(["game"]);
 
   return (
     <StyledAnswerButton
       type="button"
-      data-testid={
-        answer === correctAnswer ? "correct-answer" : `wrong-answer-${index}`
-      }
+      data-testid={ANSWER_BUTTON_COMPONENT_ID}
       $answerWasSelected={answerWasSelected}
       $isCorrectAnswer={answer === correctAnswer}
       onClick={handleClick}
@@ -48,14 +56,24 @@ export default function AnswerButton({
             alt={
               answer === correctAnswer ? t("correctAnswer") : t("wrongAnswer")
             }
+            data-testid={
+              answer === correctAnswer
+                ? ANSWER_BUTTON_COMPONENT_CORRECT_ICON_ID
+                : ANSWER_BUTTON_COMPONENT_WRONG_ICON_ID
+            }
           />
         ) : (
-          <span className="alternative-letter">
+          <span
+            className="alternative-letter"
+            data-testid={ANSWER_BUTTON_COMPONENT_LETTER_ID}
+          >
             {String.fromCharCode(index + 1 + 64)}
           </span>
         )}
       </div>
-      <span className="answer">{answer}</span>
+      <span className="answer" data-testid={ANSWER_BUTTON_COMPONENT_ANSWER_ID}>
+        {answer}
+      </span>
     </StyledAnswerButton>
   );
 }
