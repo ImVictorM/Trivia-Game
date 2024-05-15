fae109a14ff3ddbd1f1708f798b5696de6038de9
Victor Mendes <victorutdsim7@gmail.com>
2024-05-15 14:48:08 -0300

    test: feedback page

--- src/pages/Feedback/index.tsx
+++ src/pages/Feedback/index.tsx
@@ -12,6 +12,14 @@ import { useEffect } from "react";
 import usePlayerRanking from "@/hooks/usePlayerRanking";
 import { Trans, useTranslation } from "react-i18next";
 
+export const FEEDBACK_PAGE_PLAYER_SCORE_ID = "feedback-page-player-score";
+export const FEEDBACK_PAGE_PLAYER_ASSERTIONS_ID =
+  "feedback-page-player-assertions";
+export const FEEDBACK_PAGE_TEXT_ID = "feedback-page-text";
+export const FEEDBACK_PAGE_PLAYER_IMAGE_ID = "feedback-page-player-img";
+export const FEEDBACK_PAGE_PLAY_AGAIN_ID = "feedback-page-play-again";
+export const FEEDBACK_PAGE_RANKING_ID = "feedback-page-ranking";
+
 export default function Feedback() {
   const player = useSelector((state: RootState) => state.player);
   const { updateRanking } = usePlayerRanking();
@@ -36,29 +44,32 @@ export default function Feedback() {
             className="player-image"
             src={player.gravatarImgSrc}
             alt={player.name}
+            data-testid={FEEDBACK_PAGE_PLAYER_IMAGE_ID}
           />
 
-          <h1 className="feedback-title" data-testid="feedback-text">
+          <h1 className="feedback-title" data-testid={FEEDBACK_PAGE_TEXT_ID}>
             {isGoodScore ? t("goodScoreTitle") : t("badScoreTitle")}
           </h1>
 
-          <p>
+          <p data-testid={FEEDBACK_PAGE_PLAYER_ASSERTIONS_ID}>
             <Trans
               i18nKey="feedbackText"
               t={t}
               count={player.assertions}
               components={{
-                span: <span data-testid="feedback-total-question" />,
+                span: <span />,
               }}
             />
           </p>
 
-          <p>
+          <p data-testid={FEEDBACK_PAGE_PLAYER_SCORE_ID}>
             <Trans
               i18nKey="totalPoints"
               t={t}
               values={{ playerScore: player.score }}
-              components={{ span: <span data-testid="feedback-total-score" /> }}
+              components={{
+                span: <span />,
+              }}
             />
           </p>
         </StyledFeedbackCard>
@@ -68,15 +79,16 @@ export default function Feedback() {
             className="button"
             color="green"
             to="/"
-            data-testid="btn-play-again"
+            data-testid={FEEDBACK_PAGE_PLAY_AGAIN_ID}
           >
             {t("playAgain", { ns: "common" })}
           </LinkButton>
+
           <LinkButton
             className="button"
             color="cyan"
             to="/ranking"
-            data-testid="btn-ranking"
+            data-testid={FEEDBACK_PAGE_RANKING_ID}
           >
             {t("Ranking", { ns: "common" })}
           </LinkButton>
